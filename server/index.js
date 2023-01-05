dc7a6ace8513fa92179aeb504a50712f7f038fc6
dungbt-comartek <dungbt@comartek.com>
2023-01-05 23:30:14 +0700

    innit project

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://duxng17:21022001@tivi14cluster.1bvii.mongodb.net/memories?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 8000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
